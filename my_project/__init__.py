b89490fa5f267c3d3e01d75b88a850f548a5ebbb
Maxime Itasse <maxime.itasse@gmail.com>
2024-03-31 15:27:35 +0200

    test

---
+++ my_project/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for my-project."""
+import importlib.metadata
+
+
+__author__ = """Jane Doe"""
+__email__ = "jane.doe@example.com"
+__version__ = importlib.metadata.version("my_project")
---
+++ my_project/console.py
@@ -0,0 +1,22 @@
+"""Command line interface for my-project.
+
+See click documentation at https://click.palletsprojects.com/
+
+"""
+import click
+
+
+@click.group()
+@click.version_option()
+def run() -> None:
+    """Command line interface for my-project."""
+
+
+@run.command()
+def hello() -> None:
+    """Command to check my-project is correctly installed."""
+    click.echo("Welcome to my-project!")
+
+
+if __name__ == "__main__":
+    run(prog_name="my-project")  # pragma: no cover
