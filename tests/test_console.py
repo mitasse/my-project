1e36293c0cf911fdd67dc5acd49d422df15cb40a
Maxime Itasse <maxime.itasse@gmail.com>
2024-04-01 14:10:58 +0200

    test

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,20 +3,20 @@
 from click.testing import CliRunner
 
 from my_project import __version__
-from my_project.cli import cli
+from my_project import console
 
 
 def test_command_line_interface_default() -> None:
     """Test the default CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.cli)
+    result = runner.invoke(console.run)
     assert result.exit_code == 0
 
 
 def test_command_line_interface_help() -> None:
     """Test the default CLI."""
     runner = CliRunner()
-    help_result = runner.invoke(cli.cli, ["--help"])
+    help_result = runner.invoke(console.run, ["--help"])
     assert help_result.exit_code == 0
     assert "--help     Show this message and exit." in help_result.output
 
@@ -24,7 +24,7 @@ def test_command_line_interface_help() -> None:
 def test_command_line_interface_version() -> None:
     """Test the default CLI."""
     runner = CliRunner()
-    version_result = runner.invoke(cli.cli, ["--version"])
+    version_result = runner.invoke(console.run, ["--version"])
     assert version_result.exit_code == 0
     assert __version__ in version_result.output
 
@@ -32,6 +32,6 @@ def test_command_line_interface_version() -> None:
 def test_command_line_interface_hello() -> None:
     """Test the default CLI."""
     runner = CliRunner()
-    help_result = runner.invoke(cli.cli, ["hello"])
+    help_result = runner.invoke(console.run, ["hello"])
     assert help_result.exit_code == 0
     assert "Welcome to my-project!" in help_result.output
